0dce62ff877970b91f91414bb1edee983bf566e1
shalin <shalin@embla.asia>
2021-04-06 08:14:08 +0530

    ICONS AND COMPLETE EVENTS

--- Calendar/Calendar/CalendarPage.xaml.cs
+++ Calendar/Calendar/CalendarPage.xaml.cs
@@ -1,27 +1,305 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Globalization;
+using System.Linq;
+using Telerik.XamarinForms.Input;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Calendar
 {
     public partial class CalendarPage : ContentPage
     {
+        private double MainPageHeight = Application.Current.MainPage.Height;
         public CalendarPage()
         {
             InitializeComponent();
+            ViewModel = new CalendarViewModel()
+            {
+                SelectedDate = DateTime.Now
+            };
+            Calendar.SetStyleForCell = SetStyleForCell;
+        }
+        public CalendarViewModel ViewModel
+        {
+            get { return BindingContext as CalendarViewModel; }
+            set { BindingContext = value; }
+        }
+        private CalendarCellStyle SetStyleForCell(CalendarCell cell)
+        {
+
+            if (cell.Type == CalendarCellType.DayName)
+            {
+                double fontSizeDay;
+                if (Device.RuntimePlatform == Device.iOS)
+                {
+                    fontSizeDay = 10;
+                }
+                else if (Device.RuntimePlatform == Device.Android)
+                {
+                    fontSizeDay = 16;
+                }
+                else
+                {
+                    fontSizeDay = 16;
+                }
+                return new CalendarCellStyle
+                {
+                    BackgroundColor = Color.White,
+                    BorderColor = Color.Transparent,
+                    FontSize = fontSizeDay,
+
+                    // Device.OnPlatform<double>(10, 16, 16),
+                    FontAttributes = FontAttributes.Bold,
+                    TextColor = Color.FromHex("999999"),
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    VerticalTextAlignment = TextAlignment.Center,
+                    BorderThickness = 0
+                };
+            }
+
+
+            if (cell.Type == CalendarCellType.WeekNumber)
+            {
+                return new CalendarCellStyle
+                {
+                    BackgroundColor = Color.White,
+                    FontAttributes = FontAttributes.Italic,
+                    FontSize = Device.RuntimePlatform == Device.Android ? 10 : 5,
+                    TextColor = Color.Gray,
+                    VerticalTextAlignment = TextAlignment.Center,
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    BorderThickness = 0,
+                    Padding = 0
+
+                };
+            }
+
+            if (cell.Type == CalendarCellType.Title)
+            {
+                return new CalendarCellStyle
+                {
+                    FontSize = Device.RuntimePlatform == Device.Android ? 15 : 10,
+                    FontAttributes = FontAttributes.Bold,
+
+                };
+            }
+            Thickness thickness;
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                thickness = new Thickness(0, 0, 0, 1);
+            }
+            else if (Device.RuntimePlatform == Device.Android)
+            {
+                thickness = new Thickness(1);
+            }
+            else
+            {
+                thickness = new Thickness(0);
+            }
+
+            double fontSize;
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                fontSize = 15;
+            }
+            else if (Device.RuntimePlatform == Device.Android)
+            {
+                fontSize = 16;
+            }
+            else
+            {
+                fontSize = 0;
+            }
+            var defaultStyle = new CalendarCellStyle
+            {
+
+                BackgroundColor = Color.FromHex("EEEEEE"),
+                BorderColor = Color.FromHex("CCCCCC"),
+                BorderThickness = thickness, //Device.OnPlatform<Thickness>(new Thickness(0, 0, 0, 1), 1, 0),
+                FontSize = fontSize, //Device.OnPlatform<double>(15, 16, 0),
+
+                TextColor = Color.FromHex("333333")
+            };
+
+            var dayCell = cell as CalendarDayCell;
+
+            if (dayCell != null)
+            {
+                if (dayCell.Date.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    defaultStyle.TextColor = Color.FromHex("D42A28");
+                }
+
+                if (dayCell.IsFromCurrentMonth)
+                {
+                    if (dayCell.IsToday)
+                    {
+                        defaultStyle.FontAttributes = FontAttributes.Bold;
+                    }
+                }
+                else
+                {
+                    if (dayCell.IsToday)
+                    {
+                        defaultStyle.TextColor = Color.FromRgb(115, 174, 239);
+                    }
+                    else
+                    {
+                        defaultStyle.TextColor = Color.FromHex("999999");
+                        defaultStyle.BackgroundColor = Color.FromHex("E5E5E5");
+                    }
+                }
+
+                if (dayCell.IsSelected)
+                {
+                    defaultStyle.TextColor = Color.White;
+                }
+
+                return defaultStyle;
+            }
+
+            return null; // default style
         }
 
         void CalendarSelection_SelectionChanged(System.Object sender, Telerik.XamarinForms.Common.ValueChangedEventArgs<System.Int32> e)
         {
+            switch (e.NewValue)
+            {
+                case 0:
+                    Calendar.ViewMode = Telerik.XamarinForms.Input.CalendarViewMode.Day;
+                    break;
+                case 1:
+                    Calendar.ViewMode = Telerik.XamarinForms.Input.CalendarViewMode.MultiDay;
+                    break;
+                case 2:
+                    Calendar.ViewMode = Telerik.XamarinForms.Input.CalendarViewMode.Month;
+                    Calendar.WeekNumbersDisplayMode =
+                        Telerik.XamarinForms.Common.DisplayMode.Show;
+                    break;
+                case 3:
+                    Calendar.ViewMode = Telerik.XamarinForms.Input.CalendarViewMode.Week;
+                    break;
+                case 4:
+                    Calendar.ViewMode = Telerik.XamarinForms.Input.CalendarViewMode.Agenda;
+                    break;
+                default:
+                    break;
+            }
         }
+        void Calendar_ViewChanged(System.Object sender, Telerik.XamarinForms.Common.ValueChangedEventArgs<Telerik.XamarinForms.Input.CalendarViewMode> e)
+        {
+            try
+            {
+                var vm = ViewModel as CalendarViewModel;
+                if (e.NewValue == CalendarViewMode.Month || e.NewValue == CalendarViewMode.Agenda)
+                {
+                    Calendar.HeightRequest = MainPageHeight - 30;
+                    Calendar.Margin = new Thickness(0);
+                    Calendar.GridLinesDisplayMode =
+                        Telerik.XamarinForms.Common.DisplayMode.Hide;
+                    GetEvents();
+                    // TidEventsList.IsVisible = false;
+                    // EventsList.IsVisible = false;
+                }
+                else if (e.NewValue == CalendarViewMode.Day)
+                {
+                    Calendar.HeightRequest = MainPageHeight - 30;
+                    Calendar.GridLinesDisplayMode =
+                      Telerik.XamarinForms.Common.DisplayMode.Show;
+                    Calendar.WeekNumbersDisplayMode =
+                        Telerik.XamarinForms.Common.DisplayMode.Hide;
+                    if (Device.RuntimePlatform == Device.Android)
+                    {
+                        Calendar.Margin = new Thickness(-10, 0, 0, 0);
+                    }
+                    else
+                    {
+                        Calendar.Margin = new Thickness(-15, 0, 0, 0);
+                    }
+                    //  vm.ChangeEventsDefaultColor();
+                    //TidEventsList.IsVisible = false;
+                    // EventsList.IsVisible = false;
+                    //Calendar.ScrollTimeIntoView(new TimeSpan(UserProfileSettings.StartWorkingHours, 0, 0));
+                }
+                else if (e.NewValue == CalendarViewMode.MultiDay)
+                {
+                    Calendar.HeightRequest = MainPageHeight - 30;
+                    Calendar.GridLinesDisplayMode =
+                      Telerik.XamarinForms.Common.DisplayMode.Show;
+                    if (Device.RuntimePlatform == Device.Android)
+                    {
+                        Calendar.Margin = new Thickness(-15, 0, 0, 0);
+                    }
+                    else
+                    {
+                        Calendar.Margin = new Thickness(-20, 0, 0, 0);
 
+                    }
+                    // vm.ChangeEventsDefaultColor();
+                    // TidEventsList.IsVisible = false;
+                    // EventsList.IsVisible = false;
+                }
+                else if (e.NewValue == CalendarViewMode.Week)
+                {
+                    // TidEventsList.IsVisible = true;
+                    // EventsList.IsVisible = true;
+                    Calendar.GridLinesDisplayMode =
+                     Telerik.XamarinForms.Common.DisplayMode.Show;
+                    Calendar.HeightRequest = 130;
+                    MainGrid.RowDefinitions[1].Height = MainPageHeight - 30;
+                    if (vm.InitialEventList == null || !vm.InitialEventList.Any())
+                    {
+                        GetEvents();
+                    }
+                    else
+                    {
+                        //   vm.ChangeEventsDefaultColor();
+                        //   vm.FilterEventsForSelectedDate(false);
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+
+            }
+        }
         void Calendar_DisplayDateChanged(System.Object sender, Telerik.XamarinForms.Common.ValueChangedEventArgs<System.Object> e)
         {
+            if (e.NewValue == null || e.PreviousValue == null)
+            {
+                return;
+            }
+
+            CommonEventFetchDateValidation((DateTime)e.NewValue,
+           (DateTime)e.PreviousValue);
         }
 
         void Calendar_SelectionChanged(System.Object sender, Telerik.XamarinForms.Input.Calendar.CalendarSelectionChangedEventArgs<System.Object> e)
         {
+            if (e.NewValue == null)
+            {
+                return;
+            }
+            DateTime previousDateValue = DateTime.Now;
+            if (Calendar.ViewMode == CalendarViewMode.Week && e.PreviousValue == null)
+            {
+                previousDateValue = ((DateTime)e.NewValue);
+            }
+            else if (e.PreviousValue == null)
+            {
+                return;
+            }
+            else
+            {
+                previousDateValue = (DateTime)e.PreviousValue;
+            }
+
+            if (Calendar.ViewMode != CalendarViewMode.Day)
+            {
+                CommonEventFetchDateValidation((DateTime)e.NewValue,
+                previousDateValue);
+            }
         }
 
         void Calendar_TimeSlotTapped(System.Object sender, Telerik.XamarinForms.Input.TimeSlotTapEventArgs e)
@@ -32,16 +310,118 @@ namespace Calendar
         {
         }
 
-        void Calendar_ViewChanged(System.Object sender, Telerik.XamarinForms.Common.ValueChangedEventArgs<Telerik.XamarinForms.Input.CalendarViewMode> e)
-        {
-        }
-
         void Calendar_NativeControlLoaded(System.Object sender, System.EventArgs e)
         {
+            CalendarSelection.SelectedIndex = 2;
         }
 
         void Calendar_CellTapped(System.Object sender, Telerik.XamarinForms.Input.CalendarCell e)
         {
+        }
+        private async void CommonEventFetchDateValidation(DateTime newValue, DateTime preValue)
+        {
+
+            try
+            {
+                DateTime NewDateValue = newValue;
+                if (NewDateValue.Year == DateTime.MinValue.Year ||
+                    NewDateValue.Year == Calendar.MinDate.Year)
+                {
+                    return;
+                }
+                DateTime PrevDateValue = preValue;
+
+                if (Calendar.ViewMode == CalendarViewMode.Month ||
+                    Calendar.ViewMode == CalendarViewMode.Agenda)
+                {
+                    if (NewDateValue.Month != PrevDateValue.Month || NewDateValue.Year != PrevDateValue.Year)
+                    {
+                        (ViewModel as CalendarViewModel).SelectedDate = NewDateValue;
+                        GetEvents();
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                else if (Calendar.ViewMode == CalendarViewMode.MultiDay)
+                {
+                    //Calendar.DisplayDate = GetFirstDayOfWeek(NewDateValue);
+                    if ((ViewModel as CalendarViewModel).SelectedDate.Date ==
+                        NewDateValue.Date)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        (ViewModel as CalendarViewModel).SelectedDate = NewDateValue;
+                        GetEvents();
+                    }
+                }
+                else if (Calendar.ViewMode == CalendarViewMode.Week)
+                {
+                    if (PrevDateValue.Date == NewDateValue.AddDays(7).Date ||
+                        PrevDateValue.Date == NewDateValue.AddDays(-7).Date)//Week changed, display date change fired
+                    {
+                        (ViewModel as CalendarViewModel).SelectedDate = NewDateValue;
+                        GetEvents();
+                        Calendar.SelectedDate = NewDateValue;
+                    }
+                    else
+                    {
+                        (ViewModel as CalendarViewModel).SelectedDate = NewDateValue;
+                        //  (ViewModel as CalendarViewModel).FilterEventsForSelectedDate(false);
+                    }
+                }
+                else
+                {
+                    DateTime StartOfThisWeek = GetFirstDayOfWeek(PrevDateValue);
+                    DateTime EndOfThisWeek = StartOfThisWeek.AddDays(7);
+
+                    if (NewDateValue.Date < StartOfThisWeek.Date || NewDateValue.Date > EndOfThisWeek.Date)
+                    {
+                        (ViewModel as CalendarViewModel).SelectedDate = NewDateValue;
+                        GetEvents();
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                //Calendar.AppointmentsSource = (ViewModel as CalendarViewModel).EventListForCal;
+            }
+            catch (Exception exception)
+            {
+
+            }
+        }
+
+        private void GetEvents()
+        {
+
+            try
+            {
+                MainThread.BeginInvokeOnMainThread(async () =>
+                {
+                    var vm = ViewModel as CalendarViewModel;
+                    await vm.ExecuteGetMonthViewEventsCommand().ConfigureAwait(true);
+                    //Calendar.ScrollTimeIntoView(new TimeSpan(UserProfileSettings.StartWorkingHours, 0, 0));
+                });
+            }
+            catch (Exception exception)
+            {
+
+            }
+        }
+        public DateTime GetFirstDayOfWeek(DateTime dayInWeek)
+        {
+            var cultureInfo = CultureInfo.CurrentCulture;
+            DayOfWeek firstDay = cultureInfo.DateTimeFormat.FirstDayOfWeek;
+            DateTime firstDayInWeek = dayInWeek.Date;
+            while (firstDayInWeek.DayOfWeek != firstDay)
+                firstDayInWeek = firstDayInWeek.AddDays(-1);
+
+            return firstDayInWeek;
         }
     }
 }
---
+++ Calendar/Calendar/CalendarViewModel.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Telerik.XamarinForms.Input;
+using Xamarin.Forms;
+
+namespace Calendar
+{
+    public class CalendarViewModel : INotifyPropertyChanged
+    {
+        public CalendarViewModel()
+        {
+        }
+        private DateTime _selectedDate;
+        public DateTime SelectedDate
+        {
+            get { return _selectedDate; }
+            set
+            {
+                if (_selectedDate != value)
+                {
+                    _selectedDate = value;
+                    OnPropertyChanged();
+                    //UserProfileSettings.LastSelectedCalendarDate = value;
+                }
+            }
+        }
+        private ObservableCollection<DetailEventModel> _EventListForCal =
+           new ObservableCollection<DetailEventModel>();
+        public ObservableCollection<DetailEventModel> EventListForCal
+        {
+            get { return _EventListForCal; }
+            set
+            {
+                _EventListForCal = value;
+                OnPropertyChanged();
+            }
+        }
+        private ObservableCollection<DetailEventModel> _EventListForSelectedDay =
+            new ObservableCollection<DetailEventModel>();
+        public ObservableCollection<DetailEventModel> EventListForSelectedDay
+        {
+            get { return _EventListForSelectedDay; }
+            set
+            {
+                _EventListForSelectedDay = value;
+                OnPropertyChanged();
+            }
+        }
+        private ObservableCollection<DetailEventModel> _InitialEventList;
+        public ObservableCollection<DetailEventModel> InitialEventList
+        {
+            get { return _InitialEventList; }
+            set
+            {
+                _InitialEventList = value;
+                OnPropertyChanged();
+            }
+        }
+        private ObservableCollection<DetailEventModel> _InitialTidEventList;
+        public ObservableCollection<DetailEventModel> InitialTidEventList
+        {
+            get { return _InitialTidEventList; }
+            set
+            {
+                _InitialTidEventList = value;
+                OnPropertyChanged();
+            }
+        }
+        private ObservableCollection<DetailEventModel> _TIDEventListForSelectedDay =
+            new ObservableCollection<DetailEventModel>();
+        public ObservableCollection<DetailEventModel> TIDEventListForSelectedDay
+        {
+            get { return _TIDEventListForSelectedDay; }
+            set
+            {
+                _TIDEventListForSelectedDay = value;
+                OnPropertyChanged("TIDEventListForSelectedDay");
+            }
+        }
+        private CalendarViewMode _CalendarMode;
+        public CalendarViewMode CalendarMode
+        {
+            get { return _CalendarMode; }
+            set
+            {
+                _CalendarMode = value;
+                OnPropertyChanged();
+            }
+        }
+        public async Task ExecuteGetMonthViewEventsCommand()
+        {
+            try
+            {
+                if (SelectedDate == DateTime.MinValue)
+                {
+                    return;
+                }
+
+                if (SelectedDate == null)
+                {
+                    return;
+                }
+
+
+                DateTime startOfMonth;
+                DateTime endOfMonth;
+                if (CalendarMode == CalendarViewMode.Month || CalendarMode == CalendarViewMode.Agenda)
+                {
+                    startOfMonth = new DateTime(SelectedDate.Year, SelectedDate.Month, 1);
+                    endOfMonth = new DateTime(
+                        SelectedDate.Year, SelectedDate.Month,
+                        DateTime.DaysInMonth(SelectedDate.Year, SelectedDate.Month));
+                }
+                else if (CalendarMode == CalendarViewMode.MultiDay)
+                {
+                    startOfMonth = SelectedDate.Date.AddDays(-1);
+                    endOfMonth = SelectedDate.Date.AddDays(8);
+                }
+                else
+                {
+                    startOfMonth = SelectedDate.Date.AddDays(-8);
+                    endOfMonth = SelectedDate.Date.AddDays(8);
+                }
+
+
+                //await eventHelper.CreateTidSlots(SelectedResource, startOfMonth, endOfMonth, FilterDataModel.ShowTidSlots).ConfigureAwait(true);
+                //await eventHelper.GetEvents(startOfMonth, endOfMonth, SelectedLabelList, SelectedResource, SelectedFunctionList, SelectedEventStatusList, false).ConfigureAwait(true);
+                //eventHelper.UpdatePrivateAppointments(FilterDataModel.PrivateBooking);
+                //await eventHelper.ShowHideXbridgeAppointments(FilterDataModel.X_BridgeApps, startOfMonth, endOfMonth, SelectedResource).ConfigureAwait(true);
+
+                // InitialEventList = eventHelper.GetEventList();
+                // InitialTidEventList = eventHelper.GetTidEventList();
+                //FavouriteResources = eventHelper.GetFavouriteResources();
+                //FavouriteLabelList = eventHelper.GetFavLabelList();
+
+                //if (FilterDataModel != null)
+                //{
+                //    if (FilterDataModel.SpecialAppointments)
+                //    {
+                //        await eventHelper.GetAppointmentswithFunctionsmissingResourceOrSpecialAttn(startOfMonth, endOfMonth, SelectedLabelList,
+                //            SelectedResource, SelectedFunctionList, SelectedEventStatusList, false).ConfigureAwait(true);
+                //    }
+                //    else if (FilterDataModel.IncompleteAppointments)
+                //    {
+                //        await eventHelper.GetAppointmentswithFunctionsmissingResourceOrSpecialAttn(startOfMonth, endOfMonth,
+                //            SelectedLabelList, SelectedResource, SelectedFunctionList, SelectedEventStatusList, true).ConfigureAwait(true);
+                //    }
+                //    else if (FilterDataModel.AppointmentsWithStat)
+                //    {
+                //        await eventHelper.ShowHideStatCodeCountAddedAppointments(true).ConfigureAwait(true);
+                //    }
+
+                //    // InitialEventList = eventHelper.GetEventList();
+                //}
+                var startdate = SelectedDate.AddDays(-8);
+                ObservableCollection<DetailEventModel> tempList = new ObservableCollection<DetailEventModel>();
+                List<EventLabelModel> labels = new List<EventLabelModel>();
+                //labels.Add(new EventLabelModel { HexColor = "#FF0066" });
+                //labels.Add(new EventLabelModel { HexColor = "#1A4BC0" });
+                //labels.Add(new EventLabelModel { HexColor = "#804000" });
+
+                for (int i = 0; i < 50; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        tempList.Add(new DetailEventModel
+                        {
+                            Title = "Title" + i.ToString(),
+                            StartDate = startdate.AddHours(1),
+                            EndDate = startdate.AddHours(2),
+                            Color = Color.LightBlue,
+                            LocationCaption = "Room 1",
+                            EventLableList = labels
+                        });
+                    }
+                    else
+                    {
+                        tempList.Add(new DetailEventModel
+                        {
+                            Title = "Title" + i.ToString(),
+                            StartDate = startdate.AddHours(-1),
+                            EndDate = startdate.AddHours(-2),
+                            Color = Color.LightSalmon,
+                            LocationCaption = "Room 2"
+                        });
+                    }
+                    startdate = startdate.AddDays(1);
+                }
+
+                //foreach (var item in InitialTidEventList)
+                //{
+                //    tempList.Add(item);
+                //}
+
+                //foreach (var item in InitialEventList)
+                //{
+                //    if (!tempList.Any(x => x.OccurenceID == item.OccurenceID))
+                //    {
+                //        item.LableListWidth = item.EventLableList.Count * 6;
+                //        //item.Color = GetEventLabelDefaultColor(item);
+                //        tempList.Add(item);
+                //    }
+                //}
+                EventListForCal.Clear();
+                EventListForCal = tempList;
+
+                //if (CalendarMode == CalendarViewMode.Week)
+                //{
+                //    FilterEventsForSelectedDate(false);
+                //}
+            }
+            catch (Exception exception)
+            {
+
+            }
+        }
+        //private async Task GenerateEvents()
+        //{
+        //    try
+        //    {
+        //        var startdate = SelectedDate.AddDays(-8);
+        //        ObservableCollection<DetailEventModel> tempevents = new ObservableCollection<DetailEventModel>();
+        //        for (int i = 0; i < 50; i++)
+        //        {
+        //            tempevents.Add(new DetailEventModel{
+        //            });
+        //        }
+        //        return tempevents;
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //    }
+        //}
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged == null)
+                return;
+
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
