4c637851ec6e99f279e9271b6771b22f81883a2f
shalin <shalin@embla.asia>
2021-04-06 08:53:39 +0530

    ios renderers

---
+++ Calendar/Calendar.iOS/CustomCalendarDelegate.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Telerik.XamarinForms.InputRenderer.iOS;
+using TelerikUI;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using Telerik.XamarinForms.Common.iOS;
+using System.Globalization;
+namespace Calendar.iOS
+{
+    public class CustomCalendarDelegate : CalendarDelegate
+    {
+        public override void UpdateVisualsForCell(TKCalendar calendar, TKCalendarCell cell)
+        {
+            try
+            {
+                if (cell == null)
+                {
+                    return;
+                }
+                if (calendar.Presenter is TKCalendarMonthPresenter)
+                {
+                    this.SetBordersWidth(cell, 0);
+
+                    var weekkNumberCell = cell as TKCalendarWeekNumberCell;
+                    if (weekkNumberCell != null)
+                    {
+                        if (!weekkNumberCell.Label.Text.Contains("v."))
+                        {
+                            weekkNumberCell.Label.Text = "v." + weekkNumberCell.Label.Text;
+                        }
+                    }
+                }
+                else
+                {
+                    var dayTitle = cell as TKCalendarTitleCell;
+                    if (dayTitle != null)
+                    {
+                        CultureInfo cultureInfoCal = new CultureInfo(CultureInfo.CurrentCulture.Name);
+                        System.Globalization.Calendar cal = cultureInfoCal.Calendar;
+                        dayTitle.Label.Text = dayTitle.Label.Text +
+                            " (" + "week" + " " +
+                            cal.GetWeekOfYear(dayTitle.Owner.DisplayedDate.ToLocalDateTime(),
+                            cultureInfoCal.DateTimeFormat.CalendarWeekRule,
+                            cultureInfoCal.DateTimeFormat.FirstDayOfWeek) + ")";
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void SetBordersWidth(TKCalendarCell cell, int width)
+        {
+            cell.Style.TopBorderWidth = width;
+            cell.Style.LeftBorderWidth = width;
+            cell.Style.RightBorderWidth = width;
+            cell.Style.BottomBorderWidth = width;
+        }
+
+        private void SetBordersColor(TKCalendarCell cell, Color color)
+        {
+            var uiColor = color.ToUIColor();
+
+            cell.Style.TopBorderColor = uiColor;
+            cell.Style.LeftBorderColor = uiColor;
+            cell.Style.RightBorderColor = uiColor;
+            cell.Style.BottomBorderColor = uiColor;
+        }
+    }
+}
---
+++ Calendar/Calendar.iOS/DayViewDataSource.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Foundation;
+using Telerik.XamarinForms.InputRenderer.iOS;
+using TelerikUI;
+using UIKit;
+
+namespace Calendar.iOS
+{
+    public class DayViewDataSource : CalendarDayViewDataSource
+    {
+        public DayViewDataSource(CalendarRenderer renderer)
+            : base(renderer)
+        {
+        }
+        public override UICollectionViewCell EventCellForItemAtIndexPath(TKCalendarDayView dayView, NSIndexPath indexPath)
+        {
+            try
+            {
+                var cell = base.EventCellForItemAtIndexPath(dayView, indexPath);
+                var fields = cell.GetType().GetFields(
+                    BindingFlags.NonPublic | BindingFlags.Public |
+                    BindingFlags.Instance).ToList();
+                if (fields.Count > 0)
+                {
+                    var style = fields.FirstOrDefault(a => a.Name == "style");
+                    var cellStyle = style.GetValue(cell) as TKCalendarDayViewEventCellStyle;
+                    if (cellStyle != null)
+                    {
+                        cellStyle.DecorationWidth = 0;
+                    }
+                }
+
+                return cell;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
+    }
+}
